85907641fd7d77f597a4e651069c449bc232eacb
Jian Huang <alfred.huangjian@huawei.com>
2015-09-11 15:45:22 +0800

    Basic ingress point resource

--- pkg/expapi/register.go
+++ pkg/expapi/register.go
@@ -40,6 +40,8 @@ func addKnownTypes() {
 		&DaemonSet{},
 		&ThirdPartyResourceData{},
 		&ThirdPartyResourceDataList{},
+		&IngressPoint{},
+		&IngressPointList{},
 	)
 }
 
@@ -55,3 +57,5 @@ func (*DaemonSet) IsAnAPIObject()                   {}
 func (*DaemonSetList) IsAnAPIObject()               {}
 func (*ThirdPartyResourceData) IsAnAPIObject()      {}
 func (*ThirdPartyResourceDataList) IsAnAPIObject()  {}
+func (*IngressPoint) IsAnAPIObject()         	    {}
+func (*IngressPointList) IsAnAPIObject()            {}
